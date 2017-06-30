528efab1456a133409c516199dd235e0118c29b2
atom <376422905@qq.com>
2017-06-30 13:34:07 +0800

    请求参数

--- GfeduProject/app/src/main/java/jc/cici/android/atom/http/HttpPostService.java
+++ GfeduProject/app/src/main/java/jc/cici/android/atom/http/HttpPostService.java
@@ -1,21 +1,39 @@
 package jc.cici.android.atom.http;
 
+import java.util.List;
+import java.util.Map;
+
+import jc.cici.android.atom.bean.AfterInfo;
+import jc.cici.android.atom.bean.Answer;
 import jc.cici.android.atom.bean.BindJCInfo;
 import jc.cici.android.atom.bean.BindedInfo;
-import jc.cici.android.atom.bean.ClassInfo;
+import jc.cici.android.atom.bean.ClassInfoBean;
+import jc.cici.android.atom.bean.CommonBean;
 import jc.cici.android.atom.bean.DeiverInfo;
 import jc.cici.android.atom.bean.HistoryInfo;
 import jc.cici.android.atom.bean.LessInfo;
 import jc.cici.android.atom.bean.LessonInfo;
 import jc.cici.android.atom.bean.MessageBean;
+import jc.cici.android.atom.bean.MyAnswerBean;
+import jc.cici.android.atom.bean.NoteBean;
+import jc.cici.android.atom.bean.NoteDetailsBean;
 import jc.cici.android.atom.bean.PasswordInfo;
+import jc.cici.android.atom.bean.QuesDetailBean;
+import jc.cici.android.atom.bean.Question;
+import jc.cici.android.atom.bean.QuestionBean;
 import jc.cici.android.atom.bean.Register;
 import jc.cici.android.atom.bean.StageInfo;
+import jc.cici.android.atom.bean.SubjectBean;
 import jc.cici.android.atom.bean.UserInfo;
+import jc.cici.android.atom.bean.ZhuiWenBean;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -139,7 +157,7 @@ public interface HttpPostService {
      * @return
      */
     @POST("class/getmyclasslist/v500")
-    Observable<ClassInfo> getClassInfo(@Body RequestBody requestBody);
+    Observable<ClassInfoBean> getClassInfo(@Body RequestBody requestBody);
 
     /**
      * 获取阶段信息
@@ -147,8 +165,8 @@ public interface HttpPostService {
      * @param requestBody
      * @return
      */
-    @POST("/class/getclassstagelist/v500")
-    Observable<StageInfo> getStageInfo(@Body RequestBody requestBody);
+    @POST("class/getclassstagelist/v500")
+    Observable<CommonBean<StageInfo>> getStageInfo(@Body RequestBody requestBody);
 
     /**
      * 获取阶段课程信息
@@ -161,6 +179,7 @@ public interface HttpPostService {
 
     /**
      * 获取签到信息
+     *
      * @param requestBody
      * @return
      */
@@ -169,13 +188,154 @@ public interface HttpPostService {
 
     /**
      * 获取阶段历史信息
+     *
      * @param requestBody
      * @return
      */
     @POST("class/getclasshistorylessonlist/v500")
-    Observable<HistoryInfo> getHistoryLessonInfo(@Body RequestBody requestBody);
+    Observable<CommonBean<HistoryInfo>> getHistoryLessonInfo(@Body RequestBody requestBody);
 
+    /**
+     * 课程详情页
+     *
+     * @param requestBody
+     * @return
+     */
     @POST("class/getclasslessondetail/v500")
-    Observable<LessonInfo> getLessonDetailInfo(@Body RequestBody requestBody);
+    Observable<CommonBean<LessonInfo>> getLessonDetailInfo(@Body RequestBody requestBody);
+
+    /**
+     * 笔记列表接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/getnoteslist/v500")
+    Observable<CommonBean<NoteBean>> getNotesListInfo(@Body RequestBody requestBody);
+
+    /**
+     * 科目列表接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/getsubjectlist/v500")
+    Observable<CommonBean<SubjectBean>> getSubjectListInfo(@Body RequestBody requestBody);
+
+    /**
+     * 添加笔记接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/addnotes/v500")
+    Observable<CommonBean> getAddNotesInfo(@Body RequestBody requestBody);
+
+    /**
+     * 笔记详情接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/getnotesinfo/v500")
+    Observable<CommonBean<NoteDetailsBean>> getNotesDetailsInfo(@Body RequestBody requestBody);
+
+    /**
+     * 删除笔记接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/delnotes/v500")
+    Observable<CommonBean> getDelNotesInfo(@Body RequestBody requestBody);
+
+    /**
+     * 笔记点赞接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/addnotespraise/v500")
+    Observable<CommonBean> getNotesPraiseInfo(@Body RequestBody requestBody);
+
+    /**
+     * 笔记公开或私人设置接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("notes/settempval/v500")
+    Observable<CommonBean> getTempValInfo(@Body RequestBody requestBody);
+
+    /**
+     * 获取我的问题(OR 大家的问题)
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/getqueslist/v500")
+    Observable<CommonBean<QuestionBean>> getQuesListInfo(@Body RequestBody requestBody);
+
+    /**
+     * 获取我的回答列表接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/getanswerlist/v500")
+    Observable<CommonBean<MyAnswerBean>> getAnswerListInfo(@Body RequestBody requestBody);
+
+    /**
+     * 添加问题接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/addclassquesinfo/v500")
+    Observable<CommonBean<Question>> getAddClassQuesInfo(@Body RequestBody requestBody);
+
+    /**
+     * 添加我的回答接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/addclassquesinfo/v500")
+    Observable<CommonBean<Answer>> getAddAnswerInfo(@Body RequestBody requestBody);
+
+    /**
+     * 问题详情接口
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/getquesdetail/v500")
+    Observable<CommonBean<QuesDetailBean>> getQuesDetailInfo(@Body RequestBody requestBody);
+
+    /**
+     * 答案点赞功能
+     *
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/adduserpraise/v500")
+    Observable<CommonBean> addUserPraiseInfo(@Body RequestBody requestBody);
+
+    /**
+     * 答案设置为最佳
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/setclassquesstatus/v500")
+    Observable<CommonBean>setClassQuesStatusInfo(@Body RequestBody requestBody);
+
+    /**
+     * 追问(追答,评论)列表接口
+     * @param requestBody
+     * @return
+     */
+    @POST("classques/getafterquesanswerlist/v500")
+    Observable<CommonBean<ZhuiWenBean>> getAfterQuesAnswerListInfo(@Body RequestBody requestBody);
+
 
 }
